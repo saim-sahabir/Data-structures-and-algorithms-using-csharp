e961e447c5c78c06eefb78d1d82acd44311c8333
saim-sahabir <saimsahabir12it@gmail.com>
2022-06-23 18:31:31 +0600

    linklist

---
+++ LinkList/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Threading.Channels;
+
+
+
+
+
+var list = new MyListLink<string>();
+
+list.AddFirst("saim");
+list.AddFirst("hello");
+ Console.WriteLine(list);
+
+var blist = new LinkedList<string>();
+
+foreach (var item in blist)
+{
+    Console.WriteLine(item);
+}
+ 
+
+
+public class MyListLink<T>
+{
+
+    private Node<T> head;
+    private int Size = 0;
+  public class Node<T>
+    {
+       T Data; 
+      Node<T> Next;
+
+      public Node( T data)
+      {
+        this.Data = data;
+        this.Next = null;
+      }
+
+    } 
+
+    public void AddFirst(T data)
+    {
+        
+        var newNode = new Node<T>(data);
+       
+        if (head == null)
+        {
+            head = newNode;
+            
+        }
+
+        Size++;
+
+
+    }
+
+    public void AddLast(T data)
+    {
+        
+        var newNode = new Node<T>(data);
+        
+    }
+    
+    
+    
+}
+
--- String/Program.cs
+++ String/Program.cs
@@ -1,3 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+       ///////////////////////////////////////////////////////////////////////////////////////////////////////
+       // Smallest distinct window string related problem in  (Geeksforgeeks)                               //
+       // link: https://practice.geeksforgeeks.org/problems/smallest-distant-window3132/1#                  //
+       ///////////////////////////////////////////////////////////////////////////////////////////////////////
+       
+       var result = findSubString("AABBBCBBAC");
+       Console.WriteLine(result);
+       
+          int findSubString(string str)
+          {
+              var chars = str.ToCharArray();
+              int smallest = 0;
+              for(int i = 0; i < chars.Length; i++)
+              { 
+                 int count = 1;
+               for(int j = 0; j < chars.Length; j++)
+               {
+                   if(chars[i] == chars[j] && chars[i] != ' ')
+                   {
+                       count++;
+                       chars[j] = '0';
+                   }
+                  
+                   
+               }
 
-Console.WriteLine("Hello, World!");
+               if (count > 1 && chars[i] != '0')
+               {
+                   smallest++;
+               }  
+                    
+           }
+             
+             
+           return  smallest;
+       }
